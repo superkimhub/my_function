f113cf66b32caf6f8c95ce9f9b271108c18c7773
superkimhub <qasw0654@gmail.com>
2023-07-01 20:07:36 +0900

    함수 만들었어요!

---
+++ utils.py
@@ -0,0 +1,17 @@
+# 한 줄 짜리 주석
+"""
+코드 작성 일자 : 2022년 9월 8일
+코드 작성자 : 김주성
+코드 이름 : utils.py
+코드 목적 : 유용한 함수를 따로 저장하여 두고 나중에 불러와 사용하기 위함
+"""
+
+def factorial(x):
+    if x <= 1:
+        return 1
+    return x * factorial(x-1)
+
+
+def gugudan(x):
+    for i in range(9):
+        print((i + 1) * x)
